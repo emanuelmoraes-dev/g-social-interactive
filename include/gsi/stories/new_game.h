769258c498937b6feb8a6f737143e7fe640088b7
emanuelmoraes-dev <emanuelmoraes297@gmail.com>
2020-11-13 21:06:19 -0300

    add stories/new_game

---
+++ include/gsi/stories/new_game.h
@@ -0,0 +1,13 @@
+#ifndef GSI_STORIES_NEW_GAME_H_INCLUDED
+#define GSI_STORIES_NEW_GAME_H_INCLUDED
+
+#include "gsi/gsi.h"
+
+/**
+ * Obtém os dados e inicializa o jogados com os dados obtidos
+ *
+ * @param gsi instância de GSI
+ */
+void stories_init_player(GSI* gsi);
+
+#endif // GSI_STORIES_NEW_GAME_H_INCLUDED
---
+++ src/stories/new_game/console_default.c
@@ -0,0 +1,8 @@
+#include "gsi/stories/new_game.h"
+
+/**
+ * Obtém os dados e inicializa o jogados com os dados obtidos
+ *
+ * @param gsi instância de GSI
+ */
+void stories_init_player(GSI* gsi);
